e7be226ef89b17e3715b9417a28d728baea2d58f
97y1oHW <eltutbarkineltut@gmail.com>
2024-09-21 18:38:41 +0300

    Update 13413r.lua

--- 13413r.lua
+++ 13413r.lua
@@ -48,7 +48,19 @@ end
 
 local function securitylayerchecks()
 
-print("dd")
+warn("Started")
+wait(1)
+print("CHECK 1")
+wait(0.4)
+print("CHECK 2")
+wait(0.1)
+print("CHECK 3")
+wait(0.3)
+print("LAYER 4 NOT IMPLEMENTED")
+wait(0.6)
+warn("CONTINUE")
+
+
 
 end
 
@@ -227,9 +239,13 @@ local function detectExecutor()
             return "Solara"
         elseif executorName == "Nezur" then
             return "Nezur"    
+        elseif executorName == "MacSploit" then
+            return "Wave"   
         elseif executorName == "Manti" then
             return "Manti"
         elseif executorName == "Wave" then
+            return "Wave"
+        elseif executorName == "Macsploit" then
             return "Wave"
         elseif executorName == "Celery" then
             return "Celery"
@@ -263,7 +279,7 @@ local Window = Library:CreateWindow({
     -- Position and Size are also valid options here
     -- but you do not need to define them unless you are changing them :)
 
-    Title = 'Doge Hub /\ Wave PD',
+    Title = '                      Doge Hub /\ Wave PD',
     Center = true,
     AutoShow = true,
     TabPadding = 8,
@@ -1142,6 +1158,240 @@ LeftGroupBox:AddToggle('nowaterblur', {
     end
 })
 
+
+
+
+local Players = game:service("Players")
+local Player = Players.LocalPlayer
+local Mouse = Player:GetMouse()
+local Camera = game:service("Workspace").CurrentCamera
+local RS = game:service("RunService")
+local UIS = game:service("UserInputService")
+
+repeat wait() until Player.Character ~= nil and Player.Character.PrimaryPart ~= nil
+
+local LerpColorModule = loadstring(game:HttpGet("https://pastebin.com/raw/wRnsJeid"))()
+local HealthBarLerp = LerpColorModule:Lerp(Color3.fromRGB(255, 0, 0), Color3.fromRGB(0, 255, 0))
+
+local function NewCircle(Transparency, Color, Radius, Filled, Thickness)
+    local c = Drawing.new("Circle")
+    c.Transparency = Transparency
+    c.Color = Color
+    c.Visible = false
+    c.Thickness = Thickness
+    c.Position = Vector2.new(0, 0)
+    c.Radius = Radius
+    c.NumSides = math.clamp(Radius * 55 / 100, 10, 75)
+    c.Filled = Filled
+    return c
+end
+
+local RadarInfo = {
+    Position = Vector2.new(200, 200),
+    Radius = 100,
+    Scale = 1,
+    RadarBack = Color3.fromRGB(10, 10, 10),
+    RadarBorder = Color3.fromRGB(75, 75, 75),
+    LocalPlayerDot = Color3.fromRGB(255, 255, 255),
+    PlayerDot = Color3.fromRGB(60, 170, 255),
+    Team = Color3.fromRGB(0, 255, 0),
+    Enemy = Color3.fromRGB(255, 0, 0),
+    Health_Color = true,
+    Team_Check = true
+}
+
+local RadarBackground = NewCircle(0.9, RadarInfo.RadarBack, RadarInfo.Radius, true, 1)
+local RadarBorder = NewCircle(0.75, RadarInfo.RadarBorder, RadarInfo.Radius, false, 3)
+
+local function GetRelative(pos)
+    local char = Player.Character
+    if char and char.PrimaryPart then
+        local pmpart = char.PrimaryPart
+        local camerapos = Vector3.new(Camera.CFrame.Position.X, pmpart.Position.Y, Camera.CFrame.Position.Z)
+        local newcf = CFrame.new(pmpart.Position, camerapos)
+        local r = newcf:PointToObjectSpace(pos)
+        return r.X, r.Z
+    else
+        return 0, 0
+    end
+end
+
+local function PlaceDot(plr)
+    local PlayerDot = NewCircle(1, RadarInfo.PlayerDot, 3, true, 1)
+    PlayerDot.Visible = false  -- Initially hide the dot
+
+    local function Update()
+        local c
+        c = RS.RenderStepped:Connect(function()
+            if RadarBackground.Visible then  -- Check if radar is visible
+                local char = plr.Character
+                if char and char:FindFirstChildOfClass("Humanoid") and char.PrimaryPart and char:FindFirstChildOfClass("Humanoid").Health > 0 then
+                    local hum = char:FindFirstChildOfClass("Humanoid")
+                    local scale = RadarInfo.Scale
+                    local relx, rely = GetRelative(char.PrimaryPart.Position)
+                    local newpos = RadarInfo.Position - Vector2.new(relx * scale, rely * scale)
+
+                    if (newpos - RadarInfo.Position).magnitude < RadarInfo.Radius - 2 then
+                        PlayerDot.Radius = 3
+                        PlayerDot.Position = newpos
+                        PlayerDot.Visible = true
+                    else
+                        local dist = (RadarInfo.Position - newpos).magnitude
+                        local calc = (RadarInfo.Position - newpos).unit * (dist - RadarInfo.Radius)
+                        local inside = Vector2.new(newpos.X + calc.X, newpos.Y + calc.Y)
+                        PlayerDot.Radius = 2
+                        PlayerDot.Position = inside
+                        PlayerDot.Visible = true
+                    end
+
+                    PlayerDot.Color = RadarInfo.PlayerDot
+                    if RadarInfo.Team_Check then
+                        if plr.TeamColor == Player.TeamColor then
+                            PlayerDot.Color = RadarInfo.Team
+                        else
+                            PlayerDot.Color = RadarInfo.Enemy
+                        end
+                    end
+
+                    if RadarInfo.Health_Color then
+                        PlayerDot.Color = HealthBarLerp(hum.Health / hum.MaxHealth)
+                    end
+                else
+                    PlayerDot.Visible = false
+                    if Players:FindFirstChild(plr.Name) == nil then
+                        PlayerDot:Remove()
+                        c:Disconnect()
+                    end
+                end
+            else
+                PlayerDot.Visible = false
+            end
+        end)
+    end
+    coroutine.wrap(Update)()
+end
+
+for _, v in pairs(Players:GetChildren()) do
+    if v.Name ~= Player.Name then
+        PlaceDot(v)
+    end
+end
+
+local function NewLocalDot()
+    local d = Drawing.new("Triangle")
+    d.Visible = false  -- Initially hide the dot
+    d.Thickness = 1
+    d.Filled = true
+    d.Color = RadarInfo.LocalPlayerDot
+    d.PointA = RadarInfo.Position + Vector2.new(0, -6)
+    d.PointB = RadarInfo.Position + Vector2.new(-3, 6)
+    d.PointC = RadarInfo.Position + Vector2.new(3, 6)
+    return d
+end
+
+local LocalPlayerDot = NewLocalDot()
+
+Players.PlayerAdded:Connect(function(v)
+    if v.Name ~= Player.Name then
+        PlaceDot(v)
+    end
+    LocalPlayerDot:Remove()
+    LocalPlayerDot = NewLocalDot()
+end)
+
+-- Function to update radar visibility
+local function UpdateRadarVisibility(visible)
+    RadarBackground.Visible = visible
+    RadarBorder.Visible = visible
+    LocalPlayerDot.Visible = visible
+    for _, v in pairs(Players:GetChildren()) do
+        if v.Name ~= Player.Name then
+            local dot = v:FindFirstChild("RadarDot")
+            if dot then
+                dot.Visible = visible
+            end
+        end
+    end
+end
+
+-- Add the toggle to the EnemyEspTab
+if LeftGroupBox and LeftGroupBox.AddToggle then
+    LeftGroupBox:AddToggle('minimap', {
+        Text = 'Minimap',
+        Default = false,
+        Callback = function(first)
+            UpdateRadarVisibility(first)
+        end
+    })
+else
+    warn("EnemyEspTab or AddToggle method not found")
+end
+
+-- Optional: Initialize the radar visibility based on the default toggle state
+if LeftGroupBox and LeftGroupBox.GetToggleState then
+    local success, toggleState = pcall(function() return LeftGroupBox:GetToggleState('minimap') end)
+    if success then
+        UpdateRadarVisibility(toggleState)
+    else
+        warn("Failed to get toggle state for 'minimap'")
+    end
+else
+    warn("EnemyEspTab or GetToggleState method not found")
+end
+
+-- Loop
+coroutine.wrap(function()
+    local c
+    c = RS.RenderStepped:Connect(function()
+        if LocalPlayerDot then
+            LocalPlayerDot.Color = RadarInfo.LocalPlayerDot
+            LocalPlayerDot.PointA = RadarInfo.Position + Vector2.new(0, -6)
+            LocalPlayerDot.PointB = RadarInfo.Position + Vector2.new(-3, 6)
+            LocalPlayerDot.PointC = RadarInfo.Position + Vector2.new(3, 6)
+        end
+        RadarBackground.Position = RadarInfo.Position
+        RadarBackground.Radius = RadarInfo.Radius
+        RadarBackground.Color = RadarInfo.RadarBack
+
+        RadarBorder.Position = RadarInfo.Position
+        RadarBorder.Radius = RadarInfo.Radius
+        RadarBorder.Color = RadarInfo.RadarBorder
+    end)
+end)()
+
+-- Draggable
+local inset = game:service("GuiService"):GetGuiInset()
+
+local dragging = false
+local offset = Vector2.new(0, 0)
+UIS.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 and (Vector2.new(Mouse.X, Mouse.Y + inset.Y) - RadarInfo.Position).magnitude < RadarInfo.Radius then
+        offset = RadarInfo.Position - Vector2.new(Mouse.X, Mouse.Y)
+        dragging = true
+    end
+end)
+
+UIS.InputEnded:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragging = false
+    end
+end)
+
+coroutine.wrap(function()
+    local dot = NewCircle(1, Color3.fromRGB(255, 255, 255), 3, true, 1)
+    local c
+    c = RS.RenderStepped:Connect(function()
+        if (Vector2.new(Mouse.X, Mouse.Y + inset.Y) - RadarInfo.Position).magnitude < RadarInfo.Radius then
+            dot.Position = Vector2.new(Mouse.X, Mouse.Y + inset.Y)
+            dot.Visible = true
+        else
+            dot.Visible = false
+        end
+        if dragging then
+            RadarInfo.Position = Vector2.new(Mouse.X, Mouse.Y) + offset
+        end
+    end)
+end)()
 
 
 
@@ -2929,10 +3179,13 @@ ThemeManager:ApplyToTab(Tabs['UI Settings'])
 -- which has been marked to be one that auto loads!
 SaveManager:LoadAutoloadConfig()
     return -- Stop the script from executing further
+
 elseif executor == "Manti" then
     print("This script is running in Manti Executor (Not allowed)")
     plrs.LocalPlayer:Kick("\n🤥Fuck Manti Nigga")
     return
+elseif executor == "MacSploit" then
+    print("This script is running in MacSplıit")
 elseif executor == "Nezur" then
     warn("This script is running in Nezur Executor (This Executor Is Not Stable)")
 elseif executor == "Celery" then
@@ -7294,7 +7547,7 @@ pdlt.corpseespfun = function(drop)
     end))
 end
 
-Library:Notify("Doge Hub V1.5")
+Library:Notify("Doge Hub V1.5 SOLARA")
 Library:Notify(executorname33)
 Library:Notify("Optimization Loaded")
 wait(1)
