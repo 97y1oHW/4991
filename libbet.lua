ee7607ee29b59beb998cadeb8104dc153b789b26
97y1oHW <eltutbarkineltut@gmail.com>
2024-11-10 20:15:57 +0300

    Update libbet.lua

--- libbet.lua
+++ libbet.lua
@@ -2965,7 +2965,7 @@ function Library:CreateWindow(...)
     if type(Config.MenuFadeTime) ~= 'number' then Config.MenuFadeTime = 0.2 end
 
     if typeof(Config.Position) ~= 'UDim2' then Config.Position = UDim2.fromOffset(180, 50) end
-    if type(Config.Size) ~= 'UDim2' then Config.Size = UDim2.fromOffset(630, 640) end
+    if type(Config.Size) ~= 'UDim2' then Config.Size = UDim2.fromOffset(720, 750) end
 
     if Config.Center then
         Config.AnchorPoint = Vector2.new(0.5, 0.5)
