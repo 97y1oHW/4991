60fe49aede08824faf703127b020ce06359156a7
97y1oHW <eltutbarkineltut@gmail.com>
2024-11-10 20:42:56 +0300

    Update 13413r.lua

--- 13413r.lua
+++ 13413r.lua
@@ -38,6 +38,170 @@ if not LPH_OBFUSCATED then
         return print(debug.traceback()) 
     end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ---// Variables
+ Camera = game:GetService("Workspace").Camera
+ RunService = game:GetService("RunService")
+ Mouse = game:GetService("Players").LocalPlayer:GetMouse()
+ InputService = game:GetService("UserInputService")
+ TweenService = game:GetService("TweenService")
+ 
+ 
+   library = {
+    Title = 'anti.font color="rgb(645, 66, 230)">solutions</font> | <font color="rgb(245, 66, 230)">Pre-Build</font>',
+    AnimatedText = false,
+    keybind = Enum.KeyCode.End,
+    Colors = {
+        libColor = Color3.new(0.952941, 0.356863, 0.874510),
+        riskyColor = Color3.fromRGB(255, 0, 0),
+        FontColor = Color3.fromRGB(255, 255, 255),
+        MainColor = Color3.fromRGB(14, 14, 14),
+        AccentColor = Color3.new(0.952941, 0.356863, 0.874510),
+        OutlineColor = Color3.fromRGB(15, 15, 15),
+    },
+    Enabled = true,
+    colorpicking = false,
+    scrolling = true,
+    multiZindex = 200,
+    blacklisted = {
+ Enum.KeyCode.W,
+ Enum.KeyCode.A,
+ Enum.KeyCode.S,
+ Enum.KeyCode.D,
+ Enum.UserInputType.MouseMovement
+    },
+    tabbuttons = {},
+    tabs = {},
+    options = {},
+    flags = {},
+    toInvis = {},
+    Registry = {},
+    RegistryMap = {},
+    HudRegistry = {}
+ }
+  -- Menu/UI Creation
+ menu = game:GetObjects("rbxassetid://17090554797")[1] 
+ tabholder = menu.bg.bg.bg.bg.bg.bg.main.group
+ tabviewer = menu.bg.bg.bg.bg.bg.bg.tabbuttons
+ 
+     function library:Create(Class, Properties)
+ if library.Enabled == false then return end;
+ local _Instance = Class;
+ if type(Class) == 'string' then _Instance = Instance.new(Class); end;
+ for Property, Value in next, Properties do _Instance[Property] = Value; end;
+ return _Instance;
+ end;
+ ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end)
+ ScreenGui = Instance.new('ScreenGui')
+ ProtectGui(ScreenGui)
+ ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global
+ ScreenGui.Parent = game.CoreGui
+ ScreenGui.Name = "huh_menu"
+ menu.bg.pre.Text = ""
+ menu.bg.Position = UDim2.new(0.5,-menu.bg.Size.X.Offset/2,0.5,-menu.bg.Size.Y.Offset/2)
+
+
+ keynames = {
+    [Enum.KeyCode.LeftAlt] = 'LALT',
+    [Enum.KeyCode.RightAlt] = 'RALT',
+    [Enum.KeyCode.LeftControl] = 'LCTRL',
+    [Enum.KeyCode.RightControl] = 'RCTRL',
+    [Enum.KeyCode.LeftShift] = 'LSHIFT',
+    [Enum.KeyCode.RightShift] = 'RSHIFT',
+    [Enum.KeyCode.Underscore] = '_',
+    [Enum.KeyCode.Minus] = '-',
+    [Enum.KeyCode.Plus] = '+',
+    [Enum.KeyCode.Period] = '.',
+    [Enum.KeyCode.Slash] = '/',
+    [Enum.KeyCode.BackSlash] = '\\',
+    [Enum.KeyCode.Question] = '?',
+    [Enum.UserInputType.MouseButton1] = '[MB1]',
+    [Enum.UserInputType.MouseButton2] = '[MB2]',
+    [Enum.UserInputType.MouseButton3] = '[MB3]'
+ }
+ function library:GetTextBounds(Text, Font, Size, Resolution)
+    local Bounds = game:GetService('TextService'):GetTextSize(Text, Size, Font, Resolution or Vector2.new(1920, 1080))
+    return Bounds.X, Bounds.Y
+    end;
+     function library:AddToRegistry(Instance, Properties, IsHud)
+     Idx = #library.Registry + 3
+     Data = {Instance = Instance;Properties = Properties;Idx = Idx}
+    table.insert(library.Registry, Data);
+    library.RegistryMap[Instance] = Data;
+    if IsHud then table.insert(library.HudRegistry, Data) end;
+    end;
+    function library:CreateLabel(Properties, IsHud)
+ local _Instance = library:Create('TextLabel', {BackgroundTransparency = 1;Font = Enum.Font.Code;TextColor3 = library.Colors.FontColor;TextSize = 16;TextStrokeTransparency = 0});
+ library:AddToRegistry(_Instance, {TextColor3 = 'FontColor'}, IsHud);
+ return library:Create(_Instance, Properties);
+ end;
+ library.NotificationArea = library:Create('Frame', {BackgroundTransparency = 1;Position = UDim2.new(0.003, 0, 0, 40);Size = UDim2.new(0, 300, 0, 200);ZIndex = 100;Parent = ScreenGui});
+ library:Create('UIListLayout', {Padding = UDim.new(0, 4);FillDirection = Enum.FillDirection.Vertical;SortOrder = Enum.SortOrder.LayoutOrder;Parent = library.NotificationArea});
+ function library:Notify(Text, Time)
+    local XSize, YSize = library:GetTextBounds(Text, Enum.Font.Code, 14);YSize = YSize + 7
+    local NotifyOuter = library:Create('Frame', {BorderColor3 = Color3.new(189, 172, 255);Position = UDim2.new(0, 100, 0, 10);Size = UDim2.new(0, 0, 0, YSize);ClipsDescendants = true;Transparency = 0,ZIndex = 100;Parent = library.NotificationArea});
+    library:Create('UIGradient', {Color = ColorSequence.new{ColorSequenceKeypoint.new(0, library.Colors.MainColor), ColorSequenceKeypoint.new(0.1, library.Colors.MainColor), ColorSequenceKeypoint.new(0.6, library.Colors.MainColor), ColorSequenceKeypoint.new(1, library.Colors.MainColor)},Rotation = -120;Parent = NotifyOuter});
+    local NotifyInner = library:Create('Frame', {BackgroundColor3 = library.Colors.MainColor;BorderColor3 = library.Colors.OutlineColor;BorderMode = Enum.BorderMode.Inset;Size = UDim2.new(1, 0, 1, 0);ZIndex = 101;Parent = NotifyOuter});
+    local InnerFrame = library:Create('Frame', {BackgroundColor3 = Color3.new(1, 1, 1);BorderSizePixel = 0;Position = UDim2.new(0, 1, 0, 1);Size = UDim2.new(1, -2, 1, -2);ZIndex = 102;Parent = NotifyInner;});
+    local Line = library:Create('Frame', {BackgroundColor3 = library.Colors.AccentColor;BorderSizePixel = 0;Position = UDim2.new(1, 0, 0.97, 0);Size = UDim2.new(-0.999, -0.5, 0, 1.9);ZIndex = 102;Parent = NotifyInner;});
+    local LeftColor = library:Create('Frame', {BackgroundColor3 = library.Colors.AccentColor;BorderSizePixel = 0;Position = UDim2.new(0, -1, 0, 22);Size = UDim2.new(0, 2, -1.2, 0);ZIndex = 104;Parent = NotifyOuter;});
+    local Gradient = library:Create('UIGradient', {Color = ColorSequence.new({ColorSequenceKeypoint.new(0, library.Colors.MainColor),ColorSequenceKeypoint.new(1, library.Colors.MainColor)});Rotation = -90;Parent = InnerFrame});
+    library:AddToRegistry(NotifyInner, {BackgroundColor3 = 'MainColor';BorderColor3 = 'OutlineColor';}, true);
+    library:AddToRegistry(Gradient, {Color = function() return ColorSequence.new({ColorSequenceKeypoint.new(0, library.Colors.MainColor),ColorSequenceKeypoint.new(1, library.Colors.MainColor)}); end});
+    library:CreateLabel({Position = UDim2.new(0, 6, 0, 0);Size = UDim2.new(1, -4, 1, 0);Text = Text;TextXAlignment = Enum.TextXAlignment.Left;TextSize = 14;ZIndex = 103;Parent = InnerFrame});
+    pcall(NotifyOuter.TweenSize, NotifyOuter, UDim2.new(0, XSize + 8 + 4, 0, YSize), 'Out', 'Quad', 0.6, true);
+    pcall(LeftColor.TweenSize, LeftColor, UDim2.new(0, 2, 0, 0), 'Out', 'Linear', 1, true);
+    wait(0.9)
+    pcall(Line.TweenSize, Line, UDim2.new(0, 0, 0, 2), 'Out', 'Linear', Time, true);
+    task.spawn(function()
+    wait(Time or 5);
+    pcall(NotifyOuter.TweenSize, NotifyOuter, UDim2.new(0, 0, 0, YSize), 'Out', 'Quad', 0.4, true);
+    wait(0.4);
+    NotifyOuter:Destroy();
+    end);
+    end;
+
+wait(1)
+print("sent")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 print("int")
 
 local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
@@ -3077,10 +3241,10 @@ aimtab:AddToggle('nowaterblur', {
     Default = false, -- Initialize with the current state
     Callback = function(enabled)
         if enabled then
-        Library:Notify("Enabled No Water Blur")
+        library:Notify("Enabled No Water Blur", 10)
             game.Lighting.WaterBlur.Size = 0 -- If toggle is enabled, set blur to 0
         else
-        Library:Notify("Disabled No Water Blur")
+        library:Notify("Disabled No Water Blur", 10)
             game.Lighting.WaterBlur.Size = 24 -- If toggle is disabled, set blur to default (24)
         end
     end
@@ -3589,10 +3753,10 @@ local function detectBan()
             -- If BeingBanned is true, print a message
             if beingBanned == true then
                 print("You are being banned!")
-                Library:Notify("[UAC] YOU ARE GOING TO GET BANNED.")
+                library:Notify("[UAC] YOU ARE GOING TO GET BANNED.", 25)
             end
         else
-        Library:Notify("[UAC] UAC VARIABLE ERROR.")
+        library:Notify("[UAC] UAC VARIABLE ERROR.")
             warn("Could not find the UAC status for the player.")
         end
     end
@@ -3600,7 +3764,7 @@ end
 
 -- Start the ban detection coroutine
 coroutine.wrap(detectBan)()
-Library:Notify("[UAC] BAN DETECTOR STARTED!")
+library:Notify("[UAC] BAN DETECTOR STARTED!")
 
 -- Define the material mapping (for quick lookup)
 local materialMapping = {}
@@ -4407,16 +4571,16 @@ print('number_' .. tostring(counter))
 counter = counter + 1
 local charactertab = pdeltatabbox1:AddTab("misc")
  
-Library:Notify("Nexify V1.8 SOLARA")
-Library:Notify("DOWNLOADED BUG FIXES")
-Library:Notify(executorname33)
-Library:Notify("Optimization Loaded")
+library:Notify("Nexify V1.8 SOLARA")
+library:Notify("DOWNLOADED BUG FIXES")
+library:Notify(executorname33)
+library:Notify("Optimization Loaded")
 wait(1)
-Library:Notify("Loading Updates ")
+library:Notify("Loading Updates ")
 wait(0.4)
-Library:Notify("Loaded")
+library:Notify("Loaded")
 wait(0.5)
-Library:Notify("LOADED FLY SCRIPT")
+library:Notify("LOADED FLY SCRIPT")
 
 local function IsTargetVisible(target)
     if not plr.Character then return false end
@@ -5578,6 +5742,32 @@ charactertab:AddSlider('speedhack', {
 }):OnChanged(function(value)
     getgenv().speedMultiplier = value -- Set global speed value from slider
 end)
+
+
+-- Function to check if any part of the player's character is transparent
+function isPlayerTransparent(player)
+    if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+        for _, part in pairs(player.Character:GetChildren()) do
+            if part:IsA("BasePart") then
+                if part.Transparency > 0 then
+                    return true
+                end
+            end
+        end
+    end
+    return false
+end
+
+-- Listen for new players joining the game
+game.Players.PlayerAdded:Connect(function(player)
+    player.CharacterAdded:Connect(function(character)
+        -- Check if the player has any transparent parts
+        if isPlayerTransparent(player) then
+            library:Notify("MODERATOR FOUND:" ..player.Name, 30)
+        end
+    end)
+end)
+
 
 -- Set default values
 getgenv().speedMultiplier = 17
