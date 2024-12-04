d317742d64f1188997d5bf7218939a2732ab9f6e
97y1oHW <eltutbarkineltut@gmail.com>
2024-12-04 11:23:44 +0300

    Create thirdsianle.lua

---
+++ thirdsianle.lua
@@ -0,0 +1,27 @@
+local player = game:GetService("Players").LocalPlayer
+local userInputService = game:GetService("UserInputService");
+
+getgenv().thirdperson = false
+
+local function updateCamera()
+    if getgenv().thirdperson then
+        userInputService.MouseBehavior = Enum.MouseBehavior.LockCenter
+        player.CameraMaxZoomDistance = 10
+        player.CameraMinZoomDistance = 10
+        player.CameraMode = Enum.CameraMode.Classic
+        --
+        if player.Character and player.Character:FindFirstChildOfClass('Humanoid') then
+            if player.Character:FindFirstChildOfClass('Humanoid').Health > 1e-5 then
+                local root = player.Character:FindFirstChildOfClass('Humanoid').RootPart
+                root.CFrame = CFrame.new(root.CFrame.Position) * CFrame.fromOrientation(0, ({workspace.CurrentCamera.CFrame:ToOrientation()})[2], 0) 
+            end
+        end
+        workspace.CurrentCamera.CameraType = Enum.CameraType.Custom
+    else
+        player.CameraMaxZoomDistance = 0
+        player.CameraMinZoomDistance = 0
+        player.CameraMode = Enum.CameraMode.LockFirstPerson
+        workspace.CurrentCamera.CameraType = Enum.CameraType.Custom
+    end
+end
+game:GetService("RunService").PreRender:Connect(updateCamera)
