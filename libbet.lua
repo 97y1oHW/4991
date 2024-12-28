bede450f3c8feffac58cda68d7d7fa5c829b2d60
97y1oHW <eltutbarkineltut@gmail.com>
2024-12-28 14:15:29 +0300

    Update libbet.lua

--- libbet.lua
+++ libbet.lua
@@ -42,7 +42,7 @@ local Library = {
     RiskColor = Color3.fromRGB(162, 5, 5),
 
     Black = Color3.new(0, 0, 0);
-    Font = 2; 
+    Font = Enum.Font.Code; 
     OpenedFrames = {};
     DependencyBoxes = {};
 
