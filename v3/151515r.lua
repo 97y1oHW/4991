e5c23badb89db4e18a49bfbc4c772589d8a28f9c
97y1oHW <eltutbarkineltut@gmail.com>
2025-05-03 11:07:05 +0300

    Update 151515r.lua

--- v3/151515r.lua
+++ v3/151515r.lua
@@ -70,6 +70,7 @@ if not plr.Character or not plr.Character:FindFirstChild("HumanoidRootPart") the
                     Description = "Waiting for you to spawn",
                     Duration = 5
                 })
+	warn("waiting for character")
                 
     plr.CharacterAdded:Wait()
 end
