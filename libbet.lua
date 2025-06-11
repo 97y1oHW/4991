f6c7c3c1194030284a58c112fcae09b2874d2373
97y1oHW <eltutbarkineltut@gmail.com>
2025-06-11 19:13:16 +0300

    Update libbet.lua

--- libbet.lua
+++ libbet.lua
@@ -38,7 +38,7 @@ local Library = {
     RegistryMap = {};
 
     HudRegistry = {};
-
+--[[
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
@@ -46,7 +46,17 @@ local Library = {
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(162, 5, 5),
     WarnColor = Color3.fromRGB(255, 183, 14),
+]]
 
+
+FontColor = Color3.fromRGB(255, 255, 255);
+    MainColor = Color3.fromRGB(6, 6, 6);
+    BackgroundColor = Color3.fromRGB(3, 3, 3);
+    AccentColor = Color3.fromRGB(89,77,201);
+    OutlineColor = Color3.fromRGB(30, 30, 30);
+    RiskColor = Color3.fromRGB(162, 5, 5),
+    WarnColor = Color3.fromRGB(255, 183, 14),
+    
     Black = Color3.new(0, 0, 0);
     Font = Enum.Font.Code; 
     OpenedFrames = {};
